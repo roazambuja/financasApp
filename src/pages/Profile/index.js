0c56e376e29e22a2b11da1218dc53ee98db789bc
Roberta Azambuja <robertaazambuja01@gmail.com>
2021-10-25 13:26:02 -0300

    Tela de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import React, {useContext} from 'react';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+
+import { AuthContext } from '../../contexts/auth';
+import {Container, Nome, NewLink, NewText, Logout, LogoutText} from './styles';
 
 export default function Profile() {
+
+  const navigation = useNavigation();
+  const { user, signOut } = useContext(AuthContext);
+
  return (
-   <SafeAreaView>
-       <Text>Profile</Text>
-   </SafeAreaView>
+   <Container>
+      <Nome>
+        {user && user.nome}
+      </Nome>
+      <NewLink onPress={ () => navigation.navigate('Registrar')}>
+        <NewText>Registrar gastos</NewText>
+      </NewLink>
+      <Logout onPress={ () => signOut() }>
+        <LogoutText>Sair</LogoutText>
+      </Logout>
+   </Container>
   );
 }
