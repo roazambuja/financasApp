1c863746288237adade6c965f828ed9a15e1c390
Roberta Azambuja <robertaazambuja01@gmail.com>
2021-11-06 14:14:48 -0300

    Validando campos

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { SafeAreaView, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { SafeAreaView, Keyboard, TouchableWithoutFeedback, Alert } from 'react-native';
+import firebase from '../../services/firebaseConnection';
 
 import Header from '../../component/Header';
 import { Background, Input, SubmitButton, SubmitText } from './styles';
@@ -9,28 +10,55 @@ export default function New() {
   const [valor, setValor] = useState('');
   const [tipo, setTipo] = useState('receita');
 
- return (
-   <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss()}>
-    <Background>
-      <Header/>
-      <SafeAreaView style={{ alignItems: 'center'}}>
+  function handleSubmit() {
+    Keyboard.dismiss();
+    if(isNaN(parseFloat(valor)) || tipo === null) {
+      alert('Preencha todos os campos!');
+      return;
+    }
+
+    Alert.alert(
+      'Confirmando dados',
+      `Tipo ${tipo} - Valor: ${parseFloat(valor)}`,
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel'
+        },
+        {
+          text: 'Continuar',
+          onPress: () => handleAdd()
+        }
+      ]
+    )
+  }
+
+  function handleAdd() {
+    alert('ADICIONOU');
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <Background>
+        <Header />
+        <SafeAreaView style={{ alignItems: 'center' }}>
           <Input
-          placeholder="Valor desejado"
-          kayboardType="numeric"
-          returnKeyType="next"
-          onSubmitEditing={ () => Keyboard.dismiss()}
-          value={valor}
-          onChangeText={ (text) => setValor(text)}
+            placeholder="Valor desejado"
+            kayboardType="numeric"
+            returnKeyType="next"
+            onSubmitEditing={() => Keyboard.dismiss()}
+            value={valor}
+            onChangeText={(text) => setValor(text)}
           />
 
-          <Picker onChange={setTipo} tipo={tipo}/>
+          <Picker onChange={setTipo} tipo={tipo} />
 
-          <SubmitButton>
+          <SubmitButton onPress={handleSubmit}>
             <SubmitText>Registrar</SubmitText>
           </SubmitButton>
 
-      </SafeAreaView>
-    </Background>
-   </TouchableWithoutFeedback>
+        </SafeAreaView>
+      </Background>
+    </TouchableWithoutFeedback>
   );
 }
