f36c0439bbbd6c20bd8571cb846dedf3408146aa
Roberta Azambuja <robertaazambuja01@gmail.com>
2021-11-06 14:56:07 -0300

    Cadastrar novo registro

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { SafeAreaView, Keyboard, TouchableWithoutFeedback, Alert } from 'react-native';
 import firebase from '../../services/firebaseConnection';
+import { format } from 'date-fns';
+import { useNavigation} from '@react-navigation/native';
+import { AuthContext } from '../../contexts/auth';
 
 import Header from '../../component/Header';
 import { Background, Input, SubmitButton, SubmitText } from './styles';
@@ -9,6 +12,9 @@ import Picker from '../../component/Picker';
 export default function New() {
   const [valor, setValor] = useState('');
   const [tipo, setTipo] = useState('receita');
+  const { user: usuario } = useContext(AuthContext);
+
+  const navigation = useNavigation();
 
   function handleSubmit() {
     Keyboard.dismiss();
@@ -33,8 +39,29 @@ export default function New() {
     )
   }
 
-  function handleAdd() {
-    alert('ADICIONOU');
+  async function handleAdd() {
+    let uid = usuario.uid;
+    
+    let key = await firebase.database().ref('historico').child(uid).push().key;
+    await firebase.database().ref('historico').child(uid).child(key).set({
+      tipo: tipo,
+      valor: parseFloat(valor),
+      date: format(new Date(), 'dd/MM/yy')
+    })
+
+    // atualizar o saldo
+    let user = firebase.database().ref('users').child(uid);
+    await user.once('value').then((snapshot) => {
+      let saldo = parseFloat(snapshot.val().saldo);
+
+      tipo === 'despesa' ? saldo -= parseFloat(valor) : saldo += parseFloat(valor);
+      
+      user.child('saldo').set(saldo);
+
+    });
+    setValor('');
+    Keyboard.dismiss();
+    navigation.navigate('Home');
   }
 
   return (
