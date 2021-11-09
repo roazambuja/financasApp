06a10d3cce88b2dbc5642126bfb4122070c1e36d
Roberta Azambuja <robertaazambuja01@gmail.com>
2021-11-08 21:28:31 -0300

    Ver o saldo atual

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { View, Text, Button, SafeAreaView} from 'react-native';
+import firebase from '../../services/firebaseConnection';
 
 import { AuthContext } from '../../contexts/auth';
 import Header from '../../component/Header';
@@ -8,21 +9,27 @@ import HistoricoList from '../../component/HistoricoList';
 import { Background, Container, Nome, Saldo, Title, List } from './styles';
 
 export default function Home() {
-  const [historico, setHistorico] = useState([
-    {key: '1', tipo: 'receita', valor: 1200},
-    {key: '2', tipo: 'despesa', valor: 200},
-    {key: '3', tipo: 'receira', valor: 40},
-    {key: '4', tipo: 'receita', valor: 89.62},
-  ]);
+  const [historico, setHistorico] = useState([]);
+  const [saldo, setSaldo] = useState(0);
 
   const { user } = useContext(AuthContext);
+  const uid = user && user.uid;
+
+  useEffect( () => {
+    async function loadList() {
+      await firebase.database().ref('users').child(uid).on('value', (snapshot) => {
+        setSaldo(snapshot.val().saldo);
+      });
+    }
+    loadList();
+  }, []);
 
  return (
    <Background>
       <Header/>
       <Container>
         <Nome>{user && user.nome}</Nome>
-        <Saldo>R$123,00</Saldo>
+        <Saldo>R$ {saldo}</Saldo>
       </Container>
       <Title>Ultimas movimentações</Title>
       <List
