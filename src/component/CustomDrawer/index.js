fc00a04c23dd6d573ca7c101d17bd8a18a00b98c
Roberta Azambuja <robertaazambuja01@gmail.com>
2021-11-27 15:10:30 -0300

    Drawer personalizado

--- src/component/CustomDrawer/index.js
+++ src/component/CustomDrawer/index.js
@@ -1,10 +1,35 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, Image } from "react-native";
+import { DrawerContentScrollView, DrawerItemList, DrawerItem } from "@react-navigation/drawer";
 
-export default function CustomDrawer() {
+import { AuthContext} from '../../contexts/auth';
+
+export default function CustomDrawer(props) {
+    const {user, signOut} = useContext(AuthContext);
+
   return (
-    <View>
-        <Text>CustomDrawer</Text>
-    </View>
+    <DrawerContentScrollView {...props}>
+        <View style={{alignItems: 'center', justifyContent: 'center', marginTop: 25}}>
+            <Image
+                source={require('../../assets/Logo.png')}
+                style={{width: 85, height: 89}}
+                resizeMode="contain"
+            />
+
+            <Text style={{color: '#FFF', fontSize: 18, marginTop: 5}}>Bem-vindo</Text>
+            <Text style={{color: '#FFF', fontSize: 17, fontWeight: 'bold', paddingBottom: 25}}>
+                {user && user.nome}
+            </Text>
+        </View>
+
+        <DrawerItemList {...props}/>
+
+        <DrawerItem
+        {...props}
+        label="Sair do app"
+        inactiveBackgroundColor="#c62c36"
+        onPress={ () => signOut()}
+        />
+    </DrawerContentScrollView>
   );
 }
